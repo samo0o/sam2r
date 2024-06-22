1f3972d219e061575352afac3832cfcf84bfd8f6
SaMeR <41147776+samo0o@users.noreply.github.com>
2024-06-22 21:07:20 +0200

    Update logo-home.js

--- js/logo-home.js
+++ js/logo-home.js
@@ -7,20 +7,20 @@
 	var easings = ["easeOutQuad", "easeInOutQuad", "easeInOutBack", "easeOutElastic", "easeOutBounce"];
 	var values = [[20, 180, 0], [170, 170, 0], [20, 360, 0], [350, 0, 0], [0, 40, 360], [0, 320, 0], [0, 180, 0], [180, 180, 0]];
 
-	m1.colh = [100, 110, 120];
-	m2.colh = [255, 192, 0]
+	m1.data('colh', [100, 110, 120]);
+	m2.data('colh', [255, 192, 0]);
 
 	// logo hover
 	logo.hover(function () {
-		m1.logoanim(1);
-		m2.logoanim(2);
+		m1.logoanim();
+		m2.logoanim();
 	}, function () {
 		m1.velocity("reverse");
 		m2.velocity("reverse");
 	});
 
 	// logo anim prototype
-	$.fn.logoanim = function (item) {
+	$.fn.logoanim = function () {
 
 		// duration
 		var duration = Math.round(Math.random() * 400) + 200;
@@ -35,23 +35,23 @@
 
 		// velocity
 		$(this).velocity({
-			rotateX: values[a][0] * (Math.round(Math.random()) * 2 - 1),
-			rotateY: values[a][1] * (Math.round(Math.random()) * 2 - 1),
-			rotateZ: values[a][2] * (Math.round(Math.random()) * 2 - 1),
-			colorRed: this.colh[0],
-			colorGreen: this.colh[1],
-			colorBlue: this.colh[2]
+			rotateX: values[a][0] * (Math.round(Math.random()) * 2 - 1) + 'deg',
+			rotateY: values[a][1] * (Math.round(Math.random()) * 2 - 1) + 'deg',
+			rotateZ: values[a][2] * (Math.round(Math.random()) * 2 - 1) + 'deg',
+			colorRed: $(this).data('colh')[0],
+			colorGreen: $(this).data('colh')[1],
+			colorBlue: $(this).data('colh')[2]
 		}, {
-				duration: duration,
-				easing: easing
-			});
+			duration: duration,
+			easing: easing
+		});
 	}
 
 	// animate logo on document load
 	$(document).ready(function () {
-		m1.logoanim(1);
+		m1.logoanim();
 		m1.velocity("reverse");
-		m2.logoanim(2);
+		m2.logoanim();
 		m2.velocity("reverse");
 	});
 
